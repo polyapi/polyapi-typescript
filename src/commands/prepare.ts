4b541e111fe62cc3da1febfeaa86e1b473beffa6
Daniel Estoll <daestoll@gmail.com>
2025-06-17 13:16:21 -0600

    changed slice to replace

--- src/commands/prepare.ts
+++ src/commands/prepare.ts
@@ -162,12 +162,13 @@ export const prepareDeployables = async (
     );
     const staged = shell.exec('git diff --name-only --cached')
       .toString().split('\n').filter(Boolean);
+    const rootPath: string = shell.exec('git rev-parse --show-toplevel', {silent:true})
+      .toString('utf8').replace('\n', '');
     for (const deployable of dirtyDeployables) {
       try{
-        const rootPath: string = shell.exec('git rev-parse --show-toplevel', {silent:true}).toString('utf8');
-        const deployableName = deployable.file.slice(rootPath.length);
+        const deployableName = deployable.file.replace(rootPath, '');
         if (staged.includes(deployableName)) {
-          shell.echo(`Staging ${deployableName}`)
+          shell.echo(`Staging ${deployableName}`);
           shell.exec(`git add ${deployableName}`);
         }
       }
